10d0db67af39853e8dba2efe63d9eea02923a706
Rebecca Michener <mich11@umbc.edu>
2013-02-24 00:37:39 +0000

    Add first let variable

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,9 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+
+let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
   describe "Home page" do
 
     it "should have the h1 'Sample App'" do
@@ -12,8 +15,7 @@ describe "StaticPages" do
   
   it "should have the title 'Home'" do
     visit '/static_pages/home'
-    page.should have_selector('title',
-			:text => "Ruby on Rails Tutorial Sample App | Home")
+    page.should have_selector('title', :text => "#{base_title} | Home")
     end
   end
 
@@ -25,8 +27,7 @@ describe "StaticPages" do
 
     it "should have the title 'Help'" do
       visit '/static_pages/help'
-      page.should have_selector('title',
-			:text => "Ruby on Rails Tutorial Sample App | Help")								
+      page.should have_selector('title', :text => "#{base_title} | Help")								
     end
   end
 
@@ -38,8 +39,7 @@ describe "StaticPages" do
 
     it "should have the title 'About Us'" do
       visit '/static_pages/about'
-      page.should have_selector('title',
-			:text => "Ruby on Rails Tutorial Sample App | About Us")
+      page.should have_selector('title', :text => "#{base_title} | About Us")
     end
   end
  
@@ -52,8 +52,7 @@ describe "StaticPages" do
 
     it "should have the title 'Contact'" do
       visit '/static_pages/contact'
-      page.should have_selector('title',
-			:text => "Ruby on Rails Tutorial Sample App | Contact")
+      page.should have_selector('title', :text => "#{base_title} | Contact")
     end
   end
 
